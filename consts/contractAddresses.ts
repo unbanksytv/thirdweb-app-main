ce0336dcdf619a01339e76b52e3e13404870d7c4
unbanksytv <livethelife.tv@gmail.com>
2023-01-16 02:24:07 +0100

    update VIBES

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   "0x77F5D30Ee23D85eDa80c82cd3EA9161B15FFc8Bd";
 export const tokenContractAddress =
-  "0x017BB0aA8B46E49E7262098cAcBF70bBa5830fdf";
+  "0x1879068f1000Cb1A07ac948ACfcE7154608C8051";
 export const stakingContractAddress =
   "0x380038312D6b41eF86522a4b12cEC46360DD6152";
